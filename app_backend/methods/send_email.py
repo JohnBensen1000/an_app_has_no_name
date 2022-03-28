cbe77a7c5a4d6c51f29b6e3f913d5bf6437095bb
JohnBensen1000 <jbensen@purdue.edu>
2022-03-27 20:31:46 -0400

    no longer sends emails (temporary)

--- app_backend/methods/send_email.py
+++ app_backend/methods/send_email.py
@@ -7,14 +7,16 @@ port    = 465
 context = ssl.create_default_context()
 
 def send_email(bodyDict):
-    emailString = os.getenv("ENVIRONMENT") + '\n'
-    for key, value in bodyDict.items():
-        emailString += key + ': ' + value + '\n'
+    print(bodyDict)
+    pass
+    # emailString = os.getenv("ENVIRONMENT") + '\n'
+    # for key, value in bodyDict.items():
+    #     emailString += key + ': ' + value + '\n'
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("entropy.developer1@gmail.com", "CominG1$is@Winter6*9sNow11")
-        server.sendmail(
-            "entropy.developer1@gmail.com", 
-            "entropy.developer1@gmail.com", 
-            emailString
-        )
+    # with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    #     server.login("entropy.developer1@gmail.com", "tHis@@Is6**9nOt_-me")
+    #     server.sendmail(
+    #         "entropy.developer1@gmail.com", 
+    #         "entropy.developer1@gmail.com", 
+    #         emailString
+    #     )
